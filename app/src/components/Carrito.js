60293f93dcb2538d9c0518dd877d59f71ebde83e
Maximiliano Sarmiento <maximiliano.sarmiento11@gmail.com>
2022-04-06 22:33:09 -0300

    Cards Actualizadas

--- app/src/components/Carrito.js
+++ app/src/components/Carrito.js
@@ -8,51 +8,55 @@ import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
 
 const Carrito = () => {
 
-  const { carrito, borrarDelCarrito, cantidad, calcularTotalItems, calcularPrecioTotal, precioTotal, totalItems  } = useContext(contexto)
+  const { carrito, borrarDelCarrito, cantidad, calcularTotalItems, calcularPrecioTotal, precioTotal, totalItems } = useContext(contexto)
   console.log(carrito)
   console.log(totalItems)
   console.log(precioTotal)
 
-  
+
   const btnConfirmarCompra = () => {
 
-      const pedidoVenta = {
-        buyer: {
-          nombre: 'Maxi Sarmiento',
-          telefono: '123456789',
-          email: "MaxiSarmiento@cursoReact.com"
-        },
-        items: totalItems,
-        date: serverTimestamp(),
-        total: precioTotal,
-      } 
-      const ventasCollection = collection(db, "ventas");
-      const pedido = addDoc(ventasCollection, pedidoVenta )
-      console.log(pedido);
+    const pedidoVenta = {
+      buyer: {
+        nombre: 'Maxi Sarmiento',
+        telefono: '123456789',
+        email: "MaxiSarmiento@cursoReact.com"
+      },
+      items: totalItems,
+      date: serverTimestamp(),
+      total: precioTotal,
     }
-  
+    const ventasCollection = collection(db, "ventas");
+    const pedido = addDoc(ventasCollection, pedidoVenta)
+    console.log(pedido);
+  }
+
 
   return (
     <>
-    {calcularPrecioTotal()} {calcularTotalItems()}
+      {calcularPrecioTotal()} {calcularTotalItems()}
       <div className='carritoTextContainer'>
         <h2>Carrito</h2>
         {carrito.length === 0 ? <p>No hay productos en el carrito. <NavLink to="/">Por favor volve al store</NavLink> y selecciona tus productos </p> : <p>Cantidad de productos en carrito:{totalItems} .  Precio Total: ${precioTotal}</p>
         }</div>
       {
         carrito.map(elemento => (
+          <div className='carritoItemContainer'>
+            <div key={elemento.producto.id} id="cards">
+              <img src={elemento.producto.img} alt="" />
+              <div className="cardTxt">
+                <p>{elemento.producto.nombre}</p>
+                <p>{elemento.producto.precio}</p>
+                <p>Cantidad seleccionada: {elemento.cantidad}</p>
+                <p>Precio total: {elemento.producto.precio * elemento.cantidad}</p>
+              </div>
+              <Button className="btnDetalle" onClick={() => borrarDelCarrito(elemento.producto.id)}>borrar</Button>
 
-          <div key={elemento.producto.id} id="cards">
-            <img src={elemento.producto.img} alt="" />
-            <p>{elemento.producto.nombre}</p>
-            <p>{elemento.producto.precio}</p>
-            <p>Cantidad seleccionada: {elemento.cantidad}</p>
-            <p>Precio total: {elemento.producto.precio * elemento.cantidad}</p>
-            <Button className="btnDetalle" onClick={() => borrarDelCarrito(elemento.producto.id)}>borrar</Button>
+            </div>
           </div>
         )
         )}
-      {carrito.length > 0 && <Button className="btn" onClick={()=>{btnConfirmarCompra()}}>Finalizar Compra</Button>} 
+      {carrito.length > 0 && <Button className="btn" onClick={() => { btnConfirmarCompra() }}>Finalizar Compra</Button>}
     </>
   )
 }
